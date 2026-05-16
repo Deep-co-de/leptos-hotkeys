4bcff9e16e395e29d60934d1cf04c1d04ced9580
Deep-co-de <140584097+Deep-co-de@users.noreply.github.com>
2026-05-16 17:14:42 +0200

    Update use_hotkeys.rs

--- leptos_hotkeys/src/use_hotkeys.rs
+++ leptos_hotkeys/src/use_hotkeys.rs
@@ -1,4 +1,4 @@
-use leptos::{html::ElementDescriptor, prelude::*};
+use leptos::prelude::*;
 
 pub fn use_hotkeys_scoped(
     #[cfg_attr(feature = "ssr", allow(unused_variables))] key_combination: String,
